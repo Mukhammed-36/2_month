5d8a1d97c7b3c9c26e27723fa3099c16c79678bb
Mukhammed-36 <151173801+Mukhammed-36@users.noreply.github.com>
2023-11-23 00:05:09 +0600

    Add files via upload

---
+++ homework-2_6.py
@@ -0,0 +1,28 @@
+def bubble_sort(lst):
+    for i in range(len(lst)-1):
+        for j in range(len(lst)-1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+    return lst
+
+
+print(bubble_sort([10, 7, 6, 3, 6, 9]))
+
+
+def binary_search(lst, val):
+    n = len(lst)
+    left, right = 0, n - 1
+    while left <= right:
+        middle = (left + right) // 2
+        if lst[middle] == val:
+            print("Element found")
+            return middle
+        elif lst[middle] > val:
+            right = middle - 1
+        else:
+            left = middle + 1
+    print('Element wasn\'t found')
+    return -1
+
+
+print(binary_search([6, 4, 2, 3, 9, 1], 7))
